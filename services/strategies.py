da8526a6793381e1ddaf28c51d1bf1dc175d9a34
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-06-24 22:49:44 -0300

    Relocates code and adds method to expose implementation name.

--- services/strategies.py
+++ services/strategies.py
@@ -4,22 +4,14 @@ import configs
 from clients.apis import Response
 
 
-def create_strategy():
-    """
-    Provides the strategy implementation indicated on configuration.
-    :return: Strategy implementation.
-    """
-    if configs.STRATEGY_TO_USE == configs.SUM_STRATEGY:
-        return SumStrategy()
-    return AverageStrategy()
-
-
 class Strategy:
     """
     Dummy class to enforce behavior contract.
     """
-
     def coalesce(self, responses: List[Response]) -> Response:
+        raise NotImplementedError("do me")
+
+    def get_name(self) -> str:
         raise NotImplementedError("do me")
 
 
@@ -42,12 +34,14 @@ class AverageStrategy(Strategy):
         r.oop_max = int(r.stop_loss / l)
         return r
 
+    def get_name(self) -> str:
+        return configs.AVERAGE_STRATEGY
+
 
 class SumStrategy(Strategy):
     """
     Strategy implementation that resolves coalesce as sum of field values.
     """
-
     def coalesce(self, responses: List[Response]) -> Response:
         r = Response(0, 0, 0)
         for rr in responses:
@@ -55,3 +49,16 @@ class SumStrategy(Strategy):
             r.stop_loss += rr.stop_loss
             r.oop_max += rr.oop_max
         return r
+
+    def get_name(self) -> str:
+        return configs.SUM_STRATEGY
+
+
+def create_strategy() -> Strategy:
+    """
+    Provides the strategy implementation indicated on configuration.
+    :return: Strategy implementation.
+    """
+    if configs.STRATEGY_TO_USE == configs.SUM_STRATEGY:
+        return SumStrategy()
+    return AverageStrategy()
