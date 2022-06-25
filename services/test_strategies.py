8b40729968f86eb250b5d97e38219359bdf6c06c
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-06-24 22:49:45 -0300

    Adds more test units.

--- services/test_strategies.py
+++ services/test_strategies.py
@@ -1,7 +1,8 @@
 import pytest
 
+import configs
 from clients.apis import Response
-from coalesce import AverageStrategy, SumStrategy
+from strategies import create_strategy, AverageStrategy, SumStrategy
 
 
 @pytest.fixture
@@ -14,4 +15,15 @@ def test_strategies(responses):
         (AverageStrategy(), '{"deductible": 15, "stop_loss": 20, "oop_max": 6}'),
         (SumStrategy(), '{"deductible": 45, "stop_loss": 60, "oop_max": 75}'),
     ):
-        assert s.coalesce(responses).to_json() == expected
+        assert expected == s.coalesce(responses).to_json()
+
+
+def test_create_strategies():
+    for name, expected in [
+        (configs.SUM_STRATEGY, configs.SUM_STRATEGY),
+        (configs.AVERAGE_STRATEGY, configs.AVERAGE_STRATEGY),
+        ("invalid but should use the default", configs.AVERAGE_STRATEGY),
+    ]:
+        configs.STRATEGY_TO_USE = name
+        s = create_strategy()
+        assert expected == s.get_name()
