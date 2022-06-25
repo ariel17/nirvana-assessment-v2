a2beb874502a2c6aabb520af7ae00630a489a760
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2022-06-24 22:49:41 -0300

    Adds pydocs.

--- services/strategies.py
+++ services/strategies.py
@@ -5,18 +5,29 @@ from clients.apis import Response
 
 
 def create_strategy():
+    """
+    Provides the strategy implementation indicated on configuration.
+    :return: Strategy implementation.
+    """
     if configs.STRATEGY_TO_USE == configs.SUM_STRATEGY:
         return SumStrategy()
     return AverageStrategy()
 
 
 class Strategy:
+    """
+    Dummy class to enforce behavior contract.
+    """
 
     def coalesce(self, responses: List[Response]) -> Response:
         raise NotImplementedError("do me")
 
 
 class AverageStrategy(Strategy):
+    """
+    Strategy implementation where coalesce is resolved as average of values for
+    fields.
+    """
 
     def coalesce(self, responses: List[Response]) -> Response:
         r = Response(0, 0, 0)
@@ -33,6 +44,9 @@ class AverageStrategy(Strategy):
 
 
 class SumStrategy(Strategy):
+    """
+    Strategy implementation that resolves coalesce as sum of field values.
+    """
 
     def coalesce(self, responses: List[Response]) -> Response:
         r = Response(0, 0, 0)
